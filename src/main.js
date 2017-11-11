9d8f376d257075e95e05a921c291cced8b436733
yangqihua <904693433@qq.com>
2017-11-11 17:37:38 +0800

    优化

--- src/main.js
+++ src/main.js
@@ -1,21 +1,25 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import FastClick from 'fastclick'
 import VueLazyload from 'vue-lazyload'
-// import Total from './mixin/total'
 import App from './App'
 import router from './router'
 import store from './store'
 import './style/index.less'
-// require('es6-promise').polyfill()
 
-Vue.use(VueLazyload)
-// Vue.use(Total)
-FastClick.attach(document.body)
-// Vue.config.productionTip = false
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 
-/* eslint-disable no-new */
+Vue.use(VueLazyload);
+FastClick.attach(document.body);
+
+router.beforeEach((to, from, next) => {
+  NProgress.start();
+  next();
+});
+router.afterEach((to, from) => {
+  NProgress.done();
+});
+
 new Vue({
   router,
   store,
